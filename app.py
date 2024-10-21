176478b77f9401765f37e4a4f2ea58f70d89127c
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-10-21 23:15:03 +0300

    Update app.py
    changed the ports from 5500 to whatever render wants

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def index():
     return jsonify(message="Welcome to the Inventory Management API! Refer to /api for more information.")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5555)
-    print("Running")
+    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable or fallback to 5000
+    app.run(host='0.0.0.0', port=port)
